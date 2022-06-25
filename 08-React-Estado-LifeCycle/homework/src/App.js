d7f1dc51618a5e8a8c42d88fe7b990089331405b
Kevin <stiivenmendez16@gmail.com>
2022-06-25 11:05:43 -0500

    p4

--- 08-React-Estado-LifeCycle/homework/src/App.js
+++ 08-React-Estado-LifeCycle/homework/src/App.js
@@ -6,24 +6,32 @@ import Cards from './components/Cards.jsx';
 export default function App() {
   const [cities, setCities] = useState([]);
   function onSearch(ciudad) {
-    //Acá habría que hacer el llamado a la API para obtener los datos de la ciudad
-    //pero de momento agregaremos una ciudad por default para ver que funcione
+    
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+      .then(r => r.json())
+      .then((recurso) => {
+        if(recurso.main !== undefined){
+          const ciudad = {
+            min: Math.round(recurso.main.temp_min),
+            max: Math.round(recurso.main.temp_max),
+            img: recurso.weather[0].icon,
+            id: recurso.id,
+            wind: recurso.wind.speed,
+            temp: recurso.main.temp,
+            name: recurso.name,
+            weather: recurso.weather[0].main,
+            clouds: recurso.clouds.all,
+            latitud: recurso.coord.lat,
+            longitud: recurso.coord.lon
+          };
+          setCities(oldCities => [...oldCities, ciudad]);
+        } else {
+          alert("Ciudad no encontrada");
+        }
+      });
 
-    const ciudadEjemplo = {
-      min: 32,
-      max: 35,
-      img: "03n",
-      id: 2172797,
-      wind: 3.6,
-      temp: 300.15,
-      name: "Cairns",
-      weather: "Clouds",
-      clouds: 40,
-      latitud: -16.92,
-      longitud: 145.77
-    };
-    setCities(oldCities => [...oldCities, ciudadEjemplo]);
-  }
+    }
+
   return (
     <div className="App">
       <Nav onSearch={onSearch}/>
