f4dd701935e17c34551cca2d8f527f73a82c2c78
Kevin <stiivenmendez16@gmail.com>
2022-06-25 10:05:04 -0500

    p2

--- 08-React-Estado-LifeCycle/homework/src/components/Cards.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Cards.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import Card from './Card';
 import style from './Cards.css'
 
-export default function Cards(props) {
+export default function Cards({cities, onClose}) {
   // acá va tu código
   // tip, podés usar un map
+  if(cities){
   return (
     <div className={style.cards}>
-    {props.cities.map(c => 
+    {cities.map(c => 
       <Card
       max={c.main.temp_max}
       min={c.main.temp_min}
       name={c.name}
       img={c.weather[0].icon}
-      onClose={() => alert(c.name)}
-      key = {c.id}
+      onClose={() => onClose(c.id)}
+      id = {c.id}
     />)}
-    </div>)
+    </div>
+    )
+  } else {
+    return(
+      <div>Sin ciudades</div>
+    )
+  }
 };
