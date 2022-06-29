7f3aefb3efb722cccc7f5400ec0de8fdab834f40
Kevin <stiivenmendez16@gmail.com>
2022-06-29 18:04:25 -0500

    form

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+export function validate(input) {
+  let errors = {};
+  if (!input.username) {
+    errors.username = 'Username is required';
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = 'Username is invalid';
+  } else if((!/(?=.*[0-9])/.test(input.password)))
+
+  return errors;
+};
+
 
 export default function  Form() {
+  
+  const [input, setInput] = React.useState({
+    username: '',
+    password: '',
+  });
+  
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  
   return (
+    
+      <form onSubmit={handleSubmit}>
       <div>
-        Componente Form
+        <label>Username: </label>
+        <input type="text" name="username" onChange={handleInputChange} value={username} />
       </div>
+      <div>
+        <label>Password: </label>
+        <input type="text" name="password" onChange={handleInputChange} value={password} />
+      </div>
+      <input type='submit' value='submit'>Submit!</input>
+    </form>
+    
   )
 }
