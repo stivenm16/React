691efad4f96b9dd81208103592d6642e77c6ba89
Kevin <stiivenmendez16@gmail.com>
2022-06-29 09:33:28 -0500

    city done without best link

--- 09-React-Routing/homework/src/components/City.jsx
+++ 09-React-Routing/homework/src/components/City.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import './City.css';
 
 function City({cities}) {
     
     const city = cities[cities.length - 1]
+    console.log(city)
 
 
     return(
-        <div>
-            <p>{city.name}</p>
-            <p>{city.max}</p>
-            <p>{city.min}</p>
-            <p>{city.img}</p>
-            <p>{city.clouds}</p>
-            <p>{city.wind}</p>
-            <p>{city.max}</p>
-            <p>{city.longitud}</p>
-            <p>{city.latitud}</p>
-            <p>{city.temp}</p>
+        <div className="container">
+            <p className="title">{city.name}</p>
+            <div className="container-temp">
+                <p>Temp: {city.temp}°</p>
+                <p>Temp max: {city.max}°</p>
+                <p>Temp min: {city.min}°</p>
+            </div>
+            <img className="iconoClima" src={"http://openweathermap.org/img/wn/"+city.img+"@2x.png"} width="80" height="80" alt="" />    
+            <div className="container-bottom">
+                <div>
+                    <p>Wind: {city.wind}</p>
+                    <p>Clouds: {city.clouds}</p>
+                </div>
+                <div>
+                    <p>Longitud: {city.longitud}</p>
+                    <p>Latitud: {city.latitud}</p>
+                </div>
+            </div>
         </div>
     )
 }
