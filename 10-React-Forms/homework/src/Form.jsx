8001452871e9dc020af9cc226a6c3702d3114257
Kevin <stiivenmendez16@gmail.com>
2022-06-29 20:16:36 -0500

    form done

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,44 +1,74 @@
 import React from 'react';
+
 export function validate(input) {
   let errors = {};
   if (!input.username) {
     errors.username = 'Username is required';
   } else if (!/\S+@\S+\.\S+/.test(input.username)) {
     errors.username = 'Username is invalid';
-  } else if((!/(?=.*[0-9])/.test(input.password)))
+  }
+  
+  if (!input.password) {
+    errors.password = 'Password is required';
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = 'Password is invalid';
+  }
 
   return errors;
 };
 
-
 export default function  Form() {
-  
+  //username es el estado y setUsername va a ser quien modifique el estado
+  //el estado inicial de username será el valor pasado por useState
+  // El React useStateHook nos permite rastrear el estado en un componente de función
+ /*  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('') */
+
   const [input, setInput] = React.useState({
     username: '',
     password: '',
   });
-  
+
+  const [errors, setErrors] = React.useState({});
+
   const handleInputChange = function(e) {
+
+    //primero cambia estado
     setInput({
+      ...input, //se trae todo lo que habia en el objeto input
+      [e.target.name]: e.target.value //con [] entramos a la propiedad puntual del objeto
+    });
+     //llama al estado errores llamando mi funciono validate pasandole estado input recien cambiado
+    setErrors(validate({
       ...input,
       [e.target.name]: e.target.value
-    });
+    }));
   }
 
-  
   return (
-    
-      <form onSubmit={handleSubmit}>
+    <form>
       <div>
-        <label>Username: </label>
-        <input type="text" name="username" onChange={handleInputChange} value={input[username]} />
+        <label>Username:</label>                
+        <input className={errors.username && 'danger'} 
+        type="text" 
+        name="username" 
+        value={input.username} 
+        onChange={handleInputChange}/> 
+        {errors.username && (<p className="danger">{errors.username}</p> )}
       </div>
+
       <div>
-        <label>Password: </label>
-        <input type="text" name="password" onChange={handleInputChange} value={input[password]} />
+        <label>Password:</label>
+        <input className={errors.password && 'danger'} 
+        type="password" 
+        name="password" 
+        value={input.password} 
+        onChange={handleInputChange}/>
+        {errors.password && (<p className="danger">{errors.password}</p> )}
       </div>
-      <input type='submit' value='submit'>Submit!</input>
+
+      <input type="submit" value="Submit" />
+
     </form>
-    
   )
 }
