583b470c62d46c520bee8701b82f84d381d61790
Kevin <stiivenmendez16@gmail.com>
2022-07-04 14:36:48 -0500

    redux actions

--- 11-Redux/hw-js/src/reducer/index.js
+++ 11-Redux/hw-js/src/reducer/index.js
@@ -9,7 +9,17 @@ const initialState = {
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
-  
+  switch(action.type) {
+    case INCREMENTO:
+      return {
+        ...state,
+        contador: state.contador + 1
+      }
+      case DECREMENTO:
+        return {
+          contador: state.contador - 1
+        }
+  }
 }
 
 module.exports = contador;
